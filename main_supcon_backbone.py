1ede5ac1ef3173cc29e1de5ef698dd7e44cdb4ae
Sophie Haynes <s.c.haynes@rgu.ac.uk>
2024-02-12 20:54:27 +0000

    Import V2 Transforms

--- main_supcon_backbone.py
+++ main_supcon_backbone.py
@@ -10,6 +10,7 @@ import tensorboard_logger as tb_logger
 import torch
 import torch.backends.cudnn as cudnn
 from torchvision import transforms, datasets
+from torchvision.transforms import v2
 
 from util import TwoCropTransform, AverageMeter
 from util import adjust_learning_rate, warmup_learning_rate
